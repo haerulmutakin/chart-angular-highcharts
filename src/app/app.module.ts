770a238c87e4861731cb0640d02717e5a305976a
Haerul Mutakin <13523219@students.uii.ac.id>
2018-11-17 02:05:32 +0700

    enable maps

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,13 @@ import { ChartModule , HIGHCHARTS_MODULES} from 'angular-highcharts';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import * as exporting from 'highcharts/modules/exporting.src';
 import * as no_data from 'highcharts/modules/no-data-to-display.src';
 import * as highmaps from 'highcharts/modules/map.src';
 import { HttpClientModule } from '@angular/common/http';
 
 
 export function highchartsModules() {
-  return [exporting, no_data, highmaps];
+  return [no_data, highmaps];
 }
 @NgModule({
   declarations: [
